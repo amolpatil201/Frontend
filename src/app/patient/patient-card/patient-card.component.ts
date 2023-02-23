17aa5be1ef8a125c058be214c001fadd641888e0
amolpatil201 <amolpatil.im@gmail.com>
2023-02-23 11:19:07 +0530

    added patient properties

--- src/app/patient/patient-card/patient-card.component.ts
+++ src/app/patient/patient-card/patient-card.component.ts
@@ -1,17 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-patient-card',
   templateUrl: './patient-card.component.html',
   styleUrls: ['./patient-card.component.scss']
 })
-export class PatientCardComponent {
 
-  Patient: any ={
-    "Id":1,
-    "Name": "ABC Patient",
-    "Age": 40,
-    "Gender": "Male"
-  }
+export class PatientCardComponent {
+  @Input() patient : any
 
 }
--- src/app/patient/patient-list/patient-list.component.ts
+++ src/app/patient/patient-list/patient-list.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-patient-list',
   templateUrl: './patient-list.component.html',
   styleUrls: ['./patient-list.component.scss']
 })
-export class PatientListComponent {
+export class PatientListComponent implements OnInit {
+  patients: Array<any> =[
+    {
+      "Id":1,
+      "Name": "Patient 1",
+      "Age": 40,
+      "Gender": "Male"
+    },
+    {
+      "Id":2,
+      "Name": "Patient 2",
+      "Age": 20,
+      "Gender": "Female"
+    },
+    {
+      "Id":3,
+      "Name": "Patient 3",
+      "Age": 33,
+      "Gender": "Male"
+    },
+    {
+      "Id":4,
+      "Name": "Patient 4",
+      "Age": 60,
+      "Gender": "Female"
+    },
+    {
+      "Id":5,
+      "Name": "Patient 5",
+      "Age": 55,
+      "Gender": "Male"
+    },
+    {
+      "Id":6,
+      "Name": "Patient 6",
+      "Age": 30,
+      "Gender": "Female"
+    },
+    {
+      "Id":7,
+      "Name": "Patient 7",
+      "Age": 20,
+      "Gender": "Male"
+    },
 
+  ]
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
 }
